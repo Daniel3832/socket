2d6d5ccd71b0ecde5200a2481ed2cba9be15187c
daniel3832 <daniel3832@gmail.com>
2018-04-07 11:05:33 -0700

    create socket

---
+++ mysocket.py
@@ -0,0 +1,11 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print (s)
+
+server = "pythonprogramming.net"
+port = 80
+
+server_ip = socket.gethostbyname(server)
+print(server_ip)
